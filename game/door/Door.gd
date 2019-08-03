e5a5c5b3484a668930a5480a8cbb5406562ac94c
David Saltares <david.saltares@gmail.com>
2019-08-03 18:39:29 +0300

    fix door collisions

--- game/door/Door.gd
+++ game/door/Door.gd
@@ -3,16 +3,26 @@ extends KinematicBody2D
 
 const Items = preload('res://Items.gd')
 
-onready var sprite = $Sprite
+onready var sprites = [$Sprite, $Sprite2]
 onready var shape = $CollisionShape2D
 
 export(bool) var open = false setget set_open
 export(Items.Colors) var color = Items.Colors.RED
 
+var initial_layer;
+var initial_mask
+
 func _ready():
-	sprite.modulate = Items.to_color(color)
+	initial_layer = collision_layer
+	initial_mask = collision_mask
+	
+	for sprite in sprites:		
+		sprite.modulate = Items.to_color(color)
 
 func set_open(value):
 	open = value
-	sprite.visible = not open
-	shape.disabled = open
+	for sprite in sprites:
+		sprite.visible = not open
+	
+	collision_layer = 0 if open else initial_layer
+	collision_mask = 0 if open else initial_mask
