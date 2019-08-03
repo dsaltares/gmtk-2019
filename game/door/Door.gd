e53038c4efc2190322420e51bc07c9e7a8a7a957
David Saltares <david.saltares@gmail.com>
2019-08-03 19:07:44 +0300

    Add coloured effect for doors

--- game/door/Door.gd
+++ game/door/Door.gd
@@ -3,11 +3,13 @@ extends KinematicBody2D
 
 const Items = preload('res://Items.gd')
 
-onready var sprites = [$Sprite, $Sprite2]
+const red_spell = preload('res://door/red_spell.png')
+const blue_spell = preload('res://door/blue_spell.png')
+
 onready var shape = $CollisionShape2D
 
 export(bool) var open = false setget set_open
-export(Items.Colors) var color = Items.Colors.RED
+export(Items.Colors) var color = Items.Colors.RED setget set_color
 
 var initial_layer;
 var initial_mask
@@ -15,14 +17,26 @@ var initial_mask
 func _ready():
 	initial_layer = collision_layer
 	initial_mask = collision_mask
-	
-	for sprite in sprites:		
-		sprite.modulate = Items.to_color(color)
+	$Spell1/AnimationPlayer.play("idle")
+	$Spell2/AnimationPlayer.play("idle")
 
 func set_open(value):
 	open = value
-	for sprite in sprites:
-		sprite.visible = not open
+	
+	$Spell1/Sprite.visible = not open
+	$Spell2/Sprite.visible = not open
 	
 	collision_layer = 0 if open else initial_layer
 	collision_mask = 0 if open else initial_mask
+
+func set_color(new_color):
+	color = new_color
+	var texture = red_spell
+	match color:
+		Items.Colors.RED:
+			texture = red_spell
+		Items.Colors.BLUE:
+			texture = blue_spell
+	
+	$Spell1/Sprite.texture = texture
+	$Spell2/Sprite.texture = texture
